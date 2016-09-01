25c5b83ce1fb6cc68d8059446aaa5fda1f24a45a
Brett Schuchert <brett@industriallogic.com>
2016-09-01 14:00:28 -0500

    Making gitblit congituration idempotent

--- cookbooks/gitblit/recipes/default.rb
+++ cookbooks/gitblit/recipes/default.rb
@@ -6,6 +6,7 @@ bash 'install gitblit' do
     SSH_PORT=29677 
     HTTP_PORT=7765
     DEST=/opt/$APP
+    PROP=$DEST/data/gitblit.properties
     yes "" | adduser $APP &> /dev/null
     if [ ! -f /tmp/$GITBLIT.tar.gz ]; then
         wget -P /tmp http://dl.bintray.com/gitblit/releases/$GITBLIT.tar.gz
@@ -14,12 +15,19 @@ bash 'install gitblit' do
     if [ ! -f $DEST ]; then
         mkdir -p $DEST
         tar xf /tmp/$GITBLIT.tar.gz --strip-components=1 -C $DEST
-        echo "server.httpPort=$HTTP_PORT" >> $DEST/data/gitblit.properties
-        echo "git.sshPort=$SSH_PORT" >> $DEST/data/gitblit.properties
+        echo "server.httpPort=$HTTP_PORT" >> $PROP
+        echo "git.sshPort=$SSH_PORT" >> $PROP
         cd $DEST
         sh install-service-ubuntu.sh
+    else
+        sed -i "/^server.httpPort=[0-9]\+$/d" $PROP
+        sed -i "/^git.sshPort=[0-9]\+$/d" $PROP
+        echo "server.httpPort=$HTTP_PORT" >> $PROP
+        echo "git.sshPort=$SSH_PORT" >> $PROP
+        service gitblit restart
     fi
-    
+
+
     chown -R $APP:$APP $DEST
     /etc/init.d/$APP start
   EOF
